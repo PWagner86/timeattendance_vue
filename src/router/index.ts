c3a86d449c49cd75891672966ec3a93799f3df8b
Peter Wagner <peter.wagner1986@gmail.com>
2024-06-21 11:22:01 +0200

    added views and routes in index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,37 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
-})
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/evaluation',
+      name: 'Evaluation',
+      component: () => import('@/views/EvaluationView.vue')
+    },
+    {
+      path: '/person',
+      name: 'Person',
+      component: () => import('@/views/PersonView.vue')
+    },
+    {
+      path: '/project',
+      name: 'Project',
+      component: () => import('@/views/ProjectView.vue')
+    },
+    {
+      path: '/time-tracking',
+      name: 'Time tracking',
+      component: () => import('@/views/TimeTrackingView.vue')
+    }
 
-export default router
+
+  ]
+});
+
+export default router;
