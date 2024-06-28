3a278e6c4fc99c237d127dd92fe8661cec44dd58
Peter Wagner <peter.wagner1986@gmail.com>
2024-06-28 09:46:24 +0200

    added table to personview

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import PrimVue from 'primevue/config';
+import 'primeicons/primeicons.css';
 import 'primevue/resources/themes/aura-light-green/theme.css';
 
 const app = createApp(App);
--- src/views/PersonView.vue
+++ src/views/PersonView.vue
@@ -1,3 +1,35 @@
 <template>
     <h2>PersonView</h2>
+    <DataTable :value="persons" table-style="min-width: 50rem;">
+        <template #header>
+
+        </template>
+        <Column field="fullName" header="Name" />
+        <Column field="username" header="Benutzername" />
+        <Column field="role" header="Rolle" />
+        <Column field="password" header="Passwort" />
+        <Column style="min-width: 8rem;">
+            <template #body="slotProps">
+                <Button icon="pi pi-pencil" outlined rounded class="mr-2" @click="editPerson(slotProps.data)" />
+                <Button icon="pi pi-trash" outlined rounded severity="danger" @click="deletePerson(slotProps.data)" />
+            </template>
+        </Column>
+        <template #footer>Insgesamt gibt es XY Benutzer</template>
+    </DataTable>
 </template>
+
+<script setup lang="ts">
+import type { Person } from '@/types/Person';
+import { ref } from 'vue';
+
+const persons = ref<Person[]>([]);
+persons.value.push({id: 0, username: "Test", fullName: "Test User", role: "user", password: "password"});
+
+const editPerson = (p: Person) => {
+
+}
+
+const deletePerson = (p: Person) => {
+    persons.value.splice(persons.value.indexOf(p), 1);
+}
+</script>
